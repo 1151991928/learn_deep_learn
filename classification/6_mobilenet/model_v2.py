7a5b36707e36843cceac0ed8f25a353448276367
eldenring <1151991928@qq.com>
2024-11-08 16:45:21 +0800

    Create model_v2.py
    hello

---
+++ classification/6_mobilenet/model_v2.py
@@ -0,0 +1,17 @@
+from torch import nn
+import torch
+
+def _make_divisible(ch, divisor=8, min_ch=None):
+    """
+    This function is taken from the original tf repo.
+    It ensures that all layers have a channel number that is divisible by 8
+    It can be seen here:
+    https://github.com/tensorflow/models/blob/master/research/slim/nets/mobilenet/mobilenet.py
+    """
+    if min_ch is None:
+        min_ch = divisor
+    new_ch = max(min_ch, int(ch + divisor / 2) // divisor * divisor)
+    # Make sure that round down does not go down by more than 10%.
+    if new_ch < 0.9 * ch:
+        new_ch += divisor
+    return new_ch
